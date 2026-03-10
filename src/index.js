ef783e0790852f543d3b59599d06ce16abd6278b
RawBit <114392751+RawBit@users.noreply.github.com>
2026-03-10 11:15:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { AgentRuntime, ModelProviderName } from "@elizaos/core";
 import { TwitterClientInterface } from "@elizaos/plugin-twitter";
-import knafo from "../characters/knafo_xbt.character.json" with { type: "json" };
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const knafo = require("../characters/knafo_xbt.character.json");
 
 const runtime = new AgentRuntime({
   character: knafo,
